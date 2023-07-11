ec105f64c6cb45bb96a280a2835e82339d16a06b
ZubriQ <zubrich157@gmail.com>
2023-07-11 21:55:22 +0700

    refactor: minor title bar code change

--- src/PathOfHideout/MVVM/View/TitleBar.xaml.cs
+++ src/PathOfHideout/MVVM/View/TitleBar.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PathOfHideout.MVVM.View
 {
@@ -27,12 +15,12 @@ namespace PathOfHideout.MVVM.View
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
-            App.Current.Shutdown();
+            Application.Current.Shutdown();
         }
 
         private void BtnMinimize_Click(object sender, RoutedEventArgs e)
         {
-            App.Current.MainWindow.WindowState = WindowState.Minimized;
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
         }
     }
 }
