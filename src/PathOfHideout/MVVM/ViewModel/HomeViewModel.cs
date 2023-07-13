d26f1ed2a95a2684541aec4ba7d222928fd52470
ZubriQ <zubrich157@gmail.com>
2023-07-14 02:40:01 +0700

    feat: add start x, y values

--- src/PathOfHideout/MVVM/ViewModel/HomeViewModel.cs
+++ src/PathOfHideout/MVVM/ViewModel/HomeViewModel.cs
@@ -4,6 +4,6 @@ public class HomeViewModel : Core.ViewModel
 {
     public string SourcePath { get; set; } = string.Empty;
     public string DestinationPath { get; set; } = string.Empty;
-    public string XValue { get; set; } = string.Empty;
-    public string YValue { get; set; } = string.Empty;
+    public string XValue { get; set; } = "15";
+    public string YValue { get; set; } = "-15";
 }
